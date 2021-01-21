684b351b505d5f2945cd7fd8c45386a4aef9bfa0
Calum <calumforster@hotmail.com>
2021-01-21 19:54:23 +0000

    Add project files.

---
+++ SmartPTUI/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using SmartPTUI.Areas.Identity.Data;
+using SmartPTUI.Data;
+
+[assembly: HostingStartup(typeof(SmartPTUI.Areas.Identity.IdentityHostingStartup))]
+namespace SmartPTUI.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<SmartPTUIContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("SmartPTUIContextConnection")));
+
+                services.AddDefaultIdentity<SmartPTUICustomer>(options => options.SignIn.RequireConfirmedAccount = false)
+                    .AddEntityFrameworkStores<SmartPTUIContext>();
+            });
+        }
+    }
+}
---
+++ SmartPTUI/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SmartPTUI.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
