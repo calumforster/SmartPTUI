54e3597541f6e787da5ce53858d837e0faba609e
Calum Forster <calumforster@hotmail.com>
2021-03-07 16:34:27 +0000

    Added in Customers table and migrated to DB

---
+++ SmartPTUI.Data/Data/Customer.cs
@@ -0,0 +1,11 @@
+﻿
+
+namespace SmartPTUI.Data.Data
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
--- SmartPTUI.Data/Data/SmartPTUIContext.cs
+++ SmartPTUI.Data/Data/SmartPTUIContext.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using SmartPTUI.Areas.Identity.Data;
+using SmartPTUI.Data.Data;
 
 namespace SmartPTUI.Data
 {
@@ -15,9 +16,9 @@ namespace SmartPTUI.Data
         protected override void OnModelCreating(ModelBuilder builder)
         {
             base.OnModelCreating(builder);
-            // Customize the ASP.NET Identity model and override the defaults if needed.
-            // For example, you can rename the ASP.NET Identity table names and more.
-            // Add your customizations after calling base.OnModelCreating(builder);
+
         }
+
+        public DbSet<Customer> Customers { get; set; }
     }
 }
